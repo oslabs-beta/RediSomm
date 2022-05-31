1659a1587d078a5f5f66e2f06f300cf855532def
Sam <sam@TedPro13.local>
2022-05-30 17:34:13 -0700

    fixed up css a bit

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import '../../assets/logo-large.png'
 
 const Nav = () => {
   return (
     <div className="nav">
       <div className="logo-space">
-        <img className="logo-image"></img>
+        <img className="logo-image" src='../../.webpack/renderer/assets/logo-large.png'></img>
+        <div className="nav-text">
+          <ul className="nav-menu">
+            <li className="nav-menu-item" id="top-nav-item"><span className="list-text">View Databases</span></li>
+            <li className="nav-menu-item"><span className="list-text">TTL Tracker</span></li>
+            <li className="nav-menu-item"><span className="list-text">Metrics</span></li>
+          </ul>
+        </div>
       </div>
-      <h3> View Databases</h3>
-      <h3>TTL TRACKER</h3>
-      <h3 id="exit-text">出口</h3>
+      <p className="nav-menu-item" id="exit-text">出口</p>
     </div>
   );
 };
@@ -18,4 +24,5 @@ export default Nav;
 {
   /* <img src={require('../../.webpack/renderer/assets/hello.png')} /> */
 }
-//<img src={require('../../assets/logo-large.png')} />
+{/* //<img src={require('../../assets/logo-large.png')} /> */}
+
