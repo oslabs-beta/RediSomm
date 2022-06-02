73ee5180acb7c70e26689e0dcbf947497b423461
Sam <sam@TedPro13.local>
2022-06-02 16:21:28 -0700

    added dummy data and fixed ag grid responsiveness/resizing

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -16,7 +16,7 @@ export type RowDataType = {
   value: string,
   expirationTime: string,
   dataType: string,
-  expired: boolean,
+  expired: string, //this is changed from boolean to string for display purposes
   keyspaceMiss: number,
   keyspaceHit: number,
   timeAdded: Date,
@@ -28,15 +28,24 @@ export type RowDataType = {
 const Table = () => {
   const gridRef = useRef<AgGridReactType>(null);
 
-  const [rowData, setRowData] = useState<Object[]>([])
+  const [rowData, setRowData] = useState<Object[]>([
+    {key: 'data1', value: 'value1', TTL: 'no', type: 'array', status: 'false', size: '5'},
+    {key: 'data2', value: 'value2', TTL: 'no', type: 'array', status: 'false', size: '16'},
+    {key: 'data3', value: 'value3', TTL: '42069', type: 'string', status: 'false', size: '12'},
+    {key: 'Young', value: 'det. pikachu', TTL: 'no', type: 'string', status: 'false', size: '1'},
+    {key: 'Andrew', value: 'psyduck', TTL: '200', type: 'string', status: 'false', size: '6'},
+    {key: 'Andrea', value: 'charizard', TTL: 'no', type: 'string', status: 'false', size: '2'},
+    {key: 'Sam', value: 'Mr. Mime', TTL: 'n/a', type: 'string', status: 'expired', size: '31'},
+    {key: 'Eric', value: 'snorlax', TTL: 'n/a', type: 'string', status: 'expired', size: '24'},
+  ])
   const [columnDefs, setColumnDefs] = useState<Object[]>([
-    {field: 'Key'},
-    {field: 'Value'},
+    {field: 'key', resizeable: true},
+    {field: 'value'},
     {field: 'TTL'},
     //just testing;
-    {field:'Status'},
-    {field: 'Type'},
-    {field: 'Size'},
+    {field:'status'},
+    {field: 'type'},
+    {field: 'size'},
     // {field: 'keyspace misses'},
     // {field: 'Added'},
     // {field: 'del'},
@@ -47,39 +56,47 @@ const Table = () => {
     if (api === null || columnApi === null) {return;}
 
     params.api.sizeColumnsToFit()
+
   }
 
   const gridOptions: Object = {
+  
+    rowSelection: 'multiple',
+    animateRows: true
+  }
+  const defaultColDef = useMemo(()=> ({
     sortable : true,
     filter: true,
     suppressMovable: true,
     editable: true,
-    resizable: true
-  }
-
+    resizable: true,
+  }),[])
+  
   useEffect(() =>  {
     // typescript does not support useEffect returning Promises
-
-      console.log('in use effect')
-      axios('/api/getAll', {headers: {'Content-Type':'application/json'}})
-      .then(response => console.log(response))
-      .then(initData => initData)
-      .catch(err => console.log(err))
-
-      // console.log(initData)
-      // setRowData((prevState : RowDataType[]) : RowDataType[] =>{
+    
+    console.log('in use effect')
+    axios('/api/getAll', {headers: {'Content-Type':'application/json'}})
+    .then(response => console.log(response))
+    .then(initData => initData)
+    .catch(err => console.log(err))
+    
+    // console.log(initData)
+    // setRowData((prevState : RowDataType[]) : RowDataType[] =>{
       //   return [...prevState, ...initData]
       // })
-  }, [])
-
-  return (
-    <div className="table main-table ag-theme-alpine-dark">
+    }, [])
+    
+    return (
+      <div className="table main-table ag-theme-alpine-dark">
       <div className="ag-grid-main">     
       <AgGridReact 
+      animateRows={true}
+      gridOptions={gridOptions}
       ref={gridRef}
       rowData={rowData}
       columnDefs={columnDefs}
-      gridOptions={gridOptions}
+      defaultColDef={defaultColDef}
       onGridReady={onGridReady}
      />
      </div>
