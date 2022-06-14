575bd478b7f1d43ed387505574eb7732f4902d1c
Andrew Park <andrewsmacbookair@Andrews-MacBook-Air.local>
2022-06-14 12:02:43 -0700

    updating monitor controller

--- src/server/controller/monitorController.ts
+++ src/server/controller/monitorController.ts
@@ -33,21 +33,45 @@ const defaultErr = (err: Error, middlewareName: string, num = 500) => {
  *
  */
 export const monitorController: monitorController = {
+  keyspaceMissCheck: (key: any, value: any) => {
+    // check if info.keyspacemiss has increased
+    client.info((info: any) => console.log('info:', info));
+    // fetch request to mongo, increasing that key's keyspace miss value
+  },
   // turn monitor on
   onMonitorRedis: async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const monitor = new Monitor(client);
-      console.log('monitoring redis');
+      client
+      .info('stats')
+      .then((result: any) => console.log('before monitor on', result));
+      
+      client.monitor((err: Error, monitor: any): void => {
+        // Entering monitoring mode.
+        monitor.on(
+          'monitor',
+          function (
+            time: string,
+            args: string[],
+            source: string,
+            database: string
+          ) {
+            console.log(time);
+            console.log(args);
+            console.log(source);
+            console.log(database);
+            const [command, key] = args;
+            if (args[0] === 'get')
+              client
+                .info('stats')
+                .then((result: any) => console.log(result));
+          }
+        );
+      });
       return next();
     } catch (err) {
       return next(defaultErr(err, 'onMonitorRedis'));
     }
   },
 
-  offMonitorRedis: (req: Request, res: Response, next: NextFunction) => {},
-
-  keyspaceMissCheck: (key: any, value: any) => {
-    // check if info.keyspacemiss has increased
-    // fetch request to mongo, increasing that key's keyspace miss value
-  }
+  offMonitorRedis: (req: Request, res: Response, next: NextFunction) => {}
 };
